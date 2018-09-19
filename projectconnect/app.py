ccb7bfc79939503f5e3ec69d60e66f50f98b3ce5
Huy Huynh <huyqwe1993@gmail.com>
2018-09-19 11:42:38 -0700

    Add Huy's endpoint

--- projectconnect/app.py
+++ projectconnect/app.py
@@ -9,6 +9,11 @@ def home():
 @app.route('/dimitri', methods=['GET'])
 def dimitri():
     return "This is Dimitri's Endpoint"
+
+@app.route('/huyhuynh', methods=['GET'])
+def huyhuynh():
+ 	return "This is Huy's Endpoint"
+	
  
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=4000)
